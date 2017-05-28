a2383fe64450467ed85425802a5934d5dd86b15e
Bartłomiej Sielski <b.sielski@yandex.com>
2017-05-28 18:27:56 +0200

    Change labels and add a new group

--- real-names.js
+++ real-names.js
@@ -24,12 +24,12 @@ const RealNamesGroups = []
 
 RealNamesGroups.push(
     {
-	label: "Celtic names",
+	label: "Traditional Celtic names",
 	nameSets: [
 	    {
 		// http://www.namenerds.com/scottish/gaelicdude.html
 		column: "left",
-		label: "Old Scottish male",
+		label: "Scottish male",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Ailde","Ailean","Ailpein","Airril","Amhalghaidh","Angaidh","Aodh","Aodhagan","Aodhan","Aoidhean","Aonghas","Aonghasan","Armadal","Art","Artagan","Artair","Athairne","Baodan","Baoithein","Barra","Bearach","Bearchan","Beatan","Beathan","Beisdean","Blàr","Blàthan","Boisil","Bran","Branan","Brandubh","Breandan","Breannan","Brian","Brianan","Cailean","Cairbre","Caireall","Camran","Caoilte","Caorall","Carmag","Catan","Cathal","Cathalan","Cè","Ceallach","Cesan","Ciaran","Coinneach","Coireall","Coll","Colla","Còmhall","Còmhan","Conall","Conan","Conchar","Conn","Corc","Cormag","Cosgrach","Cuirealan","Curadan","Dàn","Diarmad","Diocail","Dobharcu","Dolaidh","Dòmhnallaidh","Dòmhnallan","Donaidh","Donnach","Donnan","Donnchadh","Donnchaidh","Donnghail","Drostan","Dubh","Dubhagan","Dubhshìth","Dubhthach","Dùghall","Dùghlas","Duibhne","Dunlèibhe","Eachainn","Eachann","Eacharn","Eadan","Earc","Earnan","Eircheard","Eochagan","Eochaidh","Eògan","Eoghanan","Eòghann","Eòghnaidh","Eònan","Eòran","Failbhe","Faolagan","Faolan","Fearchar","Feargan","Fearghas","Finnean","Fìonan","Fionghan","Fionn","Fionnchan","Fionnghal","Fionnlagan","Fionnlagh","Fionntan","Flaithbheartach","Flannagan","Fotadh","Friseal","Gabhran","Garbhan","Gilleanndrais","Gilleasbachan","Gilleasbaig","Gilleasp","Gilleathain","Gillebhrìghde","Gillechrìosd","Gilliosa","Gòrdan","Gormal","Guaire","Guinnein","Guirmean","Hùisdean","Iùrnan","Lachaidh","Lachann","Lachlainn","Lachlann","Laise","Latharn","Luag","Lulach","Macbeatha","Machar","Macrath","Madadh","Manachan","Maoilios","Maoldòmhnaich","Maolmoire","Maolsneachta","Maon","Mariubhe","Moireach","Muireach","Muireachan","Mungan","Murchadh","Murdaigean","Naoghas","Naoise","Naomhan","Neachdainn","Neas","Neasan","Niall","Niallghas","Nilidh","Ninean","Ninnidh","Odhran","Oisean","Oisian","Onchu","Osgar","Rodachan","Rodaidh","Ròidh","Roithridh","Rònan","Ros","Ruairidh","Ruarachan","Scotaidh","Sealbhach","Searc","Sèitheach","Sionn","Sòlas","Suibhne","Taog","Taraghlan","Taran","Teàrlach","Teàrlaidh","Torradan","Truinnean","Uallas","Ualraig","Uisdean"
@@ -38,7 +38,7 @@ RealNamesGroups.push(
 	    {
 		// source: http://www.namenerds.com/scottish/gaelic.html
 		column: "right",
-		label: "Old Scottish female",
+		label: "Scottish female",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Beathag","Borgach","Brianag","Brìghde","Caitir","Caoimhe","Caointiorn","Ceana","Ceanag","Dearbhail","Dearshul","Doileag","Dolidh","Eamhhair","Eimhir","Eithne","Fionnaghal","Fionnghuala","Gormal","Gràinne","Greudach","Latharna","Malamhìn","Malmhìn","Meadhbh","Mòr","Mòrag","Mordag","Muireall","Mùirne","Oighrig","Ròs","Seàrlaid","Sidheag","Sìleas","Slàine","Slàinidh","Slàinte","Sorcha","Teàrlag","Una",
@@ -48,7 +48,7 @@ RealNamesGroups.push(
 	    {
 		// source: http://www.namenerds.com/irish/trad.html
 		column: "left",
-		label: "Old Irish male",
+		label: "Irish male",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Maon","Miach","Séighín","Éimhín","Fachtna","Feidhlim","Ailbhe","Beag","Bearach","Cathán","Ceallach","Eirnín","Flann","Gormán","Naoise","Coinneach","Abbán","Aibhne","Ailill","Aimhirghin","Ainbheartach","Ainníleas","Anamcha","Aodh","Aodhán","Aonghus","Bairrfhionn","Baoth","Bardán","Beagán","Breacán","Breasal","Brion","Brocc","Brógán","Buadhach","Cairbre","Cairell","Calbhach","Caoimhín","Cass","Cassair","Cassán","Cathal","Cathaír","Cearbhall","Cian","Ciarán","Cillian","Cináed","Coinneach","Colmán","Comhghall","Conall","Conchobhar","Conlaodh","Conn","Conán","Cormac","Curnán","Cuán","Cúmhaí","Daigh","Daighre","Daimhín","Dallán","Deaglán","Deasmumhnach","Diarmaid","Domhnall","Donnchadh","Dubhaltach","Dubhgall","Dubhghlas","Dáire","Ealadha","Earnán","Eoghan","Faolán","Fearchar","Feardorcha","Fearghal","Fearghus","Fechín","Fiachra","Fial","Finnseach","Finnén","Fionn","Fionnbharr","Fionntán","Garbhán","Gilleagán","Giolla","Chríst","Glaisne","Gobán","Gormghiolla","Iarfhlaith","Iarlugh","Iobhar","Iomchadh","Irial","Labhraidh","Labhrás","Lachtna","Lachtnán","Laisrén","Laoidheach","Laoire","Lasair","Leannán","Lochlann","Lomán","Lonán","Lorcán","Lughaidh","Mac","Nisse","Mac","Táil","Mael","Íosa","Maeleachlainn","Mainchín","Maine","Maolán","Marcán","Meallán","Mel","Mochta","Molaisse","Morann","Muircheartach","Muireadhach","Murchadh","Murchú","Naithí","Naomhán","Neasán","Niall","Niallán","Nuadha","Odhrán","Oilithir","Oilleóg","Oillill","Oillín","Oisín","Olcán","Onchú","Orthanach","Osgar","Osán","Robhartach","Ross","Ruaidhrí","Ruarc","Ríoghán","Ríordán","Rónán","Rúadhán","Samhradhán","Scannal","Scannlán","Scoithín","Scolaí","Scáthach","Seachlann","Seanchán","Seanán","Siollán","Sléibhín","Somhairle","Suibhne","Sárán","Sé","Séadhna","Síoda","Tadhg","Taichleach","Tanaí","Tassach","Teimhnín","Tighearnach","Tighearnán","Tiobraide","Toirdhealbhach","Tomaltach","Torcán","Torna","Treasach","Tuama","Tuathal","Tíreachán","Tóla","Tómmán","Uaithne","Uallachán","Uallgarg","Ultán","Urard","Ádhamhnán","Áinle","Ámhra","Ánrothán","Éamonn","Éanna","Ógán","Ólchobhar"
@@ -57,7 +57,7 @@ RealNamesGroups.push(
 	    {
 		// source: http://www.namenerds.com/irish/trad.html
 		column: "right",
-		label: "Old Irish female",
+		label: "Irish female",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Aillean","Ainnir","Aithche","Ana","Aodhamair","Aodnait","Aoibheann","Aoibhegréine","Aoife","Banbha","Blinne","Bláthnaid","Bláthín","Bríghid","Brónach","Báine","Bébhinn","Caoilfhionn","Caoimhe","Ceara","Ciar","Clíodhna","Dairinn","Damhnait","Dearbhail","Deirbhile","Deirdre","Dunfhlaith","Dáirine","Dáirinn","Eadan","Easnadh","Echna","Eibhear","Eithne","Fainche","Feidhelm","Feme","Fianait","Fionnait","Fionnghuala","Fionúir","Féthnaid","Geiléis","Gobnait","Gormlaith","Grian","Gráinne","Laoise","Lasairfhiona","Liamhain","Life","Luiseach","Líadan","Líobhan","Marga","Meadhbh","Meallá","Moinnine","Moncha","Muadhnait","Muireann","Muirgheal","Muirne","Muirín","Móirne","Mór","Naomh","Neacht","Neamhain","Neasa","Niamh","Nuala","Nárbhflaith","Osnait","Rathnait","Ríofach","Ríona","Róinseach","Róisín","Rónnad","Rós","Rúadhnait","Sadhbh","Saorla","Saraid","Scoithniamh","Scoth","Scothnait","Searc","Sláine","Sorcha","Suaibhseach","Suanach","Sárnait","Séanait","Síomha","Taillte","Teafa","Teamhair","Treasa","Tuamnait","Tuathla","Tuileach","Tuilelaith","Téide","Uaine","Uaine","Uainionn","Uallach","Uasal","Áine","Áinfean","Álmhath","Éibhleann","Éile","Éirne","Émer","Étaín","Íde","Íonait","Órlaith","Órnait","Úna","Maon","Miach","Séighín","Éimhín","Fachtna","Feidhlim","Ailbhe","Beag","Bearach","Cathán","Ceallach","Eirnín","Flann","Gormán","Naoise","Coinneach"
@@ -66,7 +66,7 @@ RealNamesGroups.push(
 	    {
 		// source: http://www.namenerds.com/welsh/trad2.html
 		column: "left",
-		label: "Old Welsh male",
+		label: "Welsh male",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Adda","Addaon","Adeon","Aedd","Aeddan","Aelhearn","Aelwyn","Aeron","Afagddu","Afallon","Afan","Alan","Alaw","Alawn","Alban","Aldith","Aldryd","Alec","Aled","Alun","Alwyn","Amaethon","Amlodd","Anarawd","Andreas","Aneirin","Angwyn","Anhun","Arawn","Arial","Arianwyn","Arnallt","Arofan","Aron","Arthen","Arthfael","Arthgen","Arthur","Arwel","Arwyn","Asaff","Aurfryn","Auryn","Awen","Awsten","Baeddan","Baglan","Barri","Barti","Bedaws","Bedwyn","Bedivere","Bedwyn","Bedwyr","Berddig","Bergam","Berwyn","Bevan","Beuno","Beynon","Bleddyn","Bledig","Bledri","Blwchfardd","Boreugwyn","Brân","Branwaladr","Breichiawl","Brice","Brioc","Brochfael","Brychan","Bryn","Brynllyn","Brynmor","Bryngwyn","Brython","Cadell","Cadfael","Cadog","Cadwaladr","Cadwallon","Cadwgan","Caerwyn","Caradog","Carwyn","Caswallon","Cawrdaf","Cecil","Cedrych","Cei","Ceinfryn","Ceiriog","Celyn","Celynen","Cennydd","Ceredig","Ceri","Cewydd","Cian","Cledwyn","Clwyd","Clydai","Clydno","Coel","Collen","Colwyn","Conan","Conwy","Cospatrick","Craigfryn","Cranog","Crwys","Culhwch","Cunedda","Curig","Cyfeiliog","Cyffin","Cymro","Cynan","Cynddelw","Cyndeyrn","Cynddylan","Cynedda","Cynfab","Cynfarch","Cynfael","Cynfrig","Cynog","Cynon","Dafydd","Dai","Dedwydd","Deian","Deiniol","Delwyn","Derfel","Deri","Dewi","Dogmael","Dwyryd","Dyfri","Dylan","Ednyfed","Einion","Eirian","Eirwyn","Elffin","Elidir","Elis","Elwyn","Elystan","Emlyn","Emrys","Emyr","Evan","Fflamddwyn","Ffransis","Fychan","Gareth","Gawain","Geraint","Gerallt","Gethin","Gildas","Gilfaethwy","Glanmor","Glyn","Goronwy","Grigor","Gruffydd","Gwaednerth","Gwalchgwyn","Gwalchmai","Gwatcyn","Gwaun","Gwenlyn","Gwili","Gwilym","Gwion","Gwydderig","Gwyddno","Gwydion","Gwyn","Gwynfor","Gwythyr","Hafgan","Heddwyn","Heilyn","Heini","Hergest","Heulfryn","Heulyn","Huw","Hywel","Iago","Ianto","Idris","Idwal","Iestin","Ieuan","Ifan","Ifor","Illtyd","Ioan","Iolo","Iorweth","Lewys","Llew","Lloyd","Llyr","Llywarch","Llywelyn","Mabon","Macsen","Madog","Mael","Maelgwyn","Maelgwn","Maelmadog","Math","Mathonwy","Medrawd","Meical","Meilyr","Meirion","Meredudd","Merfyn","Meurig","Mihangel","Moelwyn","Morgan","Morien","Myrddin","Nudd","Owain","Pawl","Pedr","Peredur","Powel","Pryderi","Pwyll","Rhein","Rheinallt","Rhisiart","Rhodri","Rhun","Rhydderch","Rhydian","Rhydwyn","Rhys","Robat","Seiriol","Selwyn","Siarl","Siencyn","Simwnt","Sion","Sionyn","Sior","Steffan","Sulien","Sulwyn","Taliesin","Tegwyn","Teilo","Tewdwr","Tomos","Trefor","Trystan","Wyn","Ynyr"
@@ -75,7 +75,7 @@ RealNamesGroups.push(
 	    {
 		// source: http://www.namenerds.com/welsh/trad1.html
 		column: "right",
-		label: "Old Welsh female",
+		label: "Welsh female",
 		splitters: [splitterAfter, splitterBefore],
 		filters: standardFilters,
 		names: ["Addfwyn","Aderyn","Adlais","Adwen","Aelwen","Aerfen","Aeron","Aeronwen","Aeronwy","Afryl","Alaw","Aldyth","Alwen","Alys","Amranwen","Aneira","Anest","Angharad","Ann","Anna","Anwen","Annwr","Annwyl","Arddun","Arianell","Arianrhod","Arianwen","Arwen","Aurddolen","Auron","Awela","Awena","Banwen","Beca","Berwen","Bethan","Beti","Betrys","Blodeuedd","Blodwedd","Blodwen","Branwen","Brengwain","Briallen","Brisen","Bronmai","Bronwen","Brythonwen","Buddug","Cadi","Cain","Calan","Canaid","Caron","Caronwen","Carwen","Caryl","Carys","Catrin","Ceindeg","Ceindrych","Ceinfryn","Ceinwen","Ceirios","Ceri","Ceridwen","Cerys","Collwen","Creuddylad","Crisiant","Crystyn","Cyffin","Daron","Deilwen","Del","Delun","Delwen","Delyn","Delyth","Deris","Derwena","Deryn","Deryth","Dilwen","Dilys","Drudwen","Drysi","Dwyfor","Dwynwen","Dwyryd","Dwysan","Dyddanwy","Dyddgu","Dyfi","Ebrill","Edwy","Efa","Eigra","Eilir","Eilonwy","Eilwen","Eira","Eirian","Eirianwen","Eirianedd","Eiriol","Eirlys","Eirwen","Eirys","Elan","Elain","Elen","Elenid","Eleri","Elinor","Elisabeth","Elli","Elliw","Eluned","Enfys","Enid","Eos","Erin","Eryl","Esyllt","Ethni","Eurfron","Euron","Eurddolyn","Eurwen","Ewig","Faleiry","Fanw","Ffion","Fflur","Ffraid","Ffreuer","Fioled","Gaenor","Geinor","Generys","Glenda","Glenys","Glesni","Glynis","Goleubryd","Goleudydd","Grug","Gwawr","Gwawrddydd","Gwawrwen","Gwen","Gwenda","Gwenddydd","Gwendolen","Gwener","Gwenfair","Gwenfor","Gwenfrewi","Gwenfron","Gwenhwyfar","Gwenith","Gwenllian","Gwenlyn","Gwennan","Gwennant","Gwennol","Gwenol","Gwenydd","Gwenynan","Gwerful","Gwernen","Gwernfyl","Gwlithen","Gwyddelan","Gwyddfid","Gwladys","Gwylan","Gwylfai","Gwyneth","Gwynonwen","Haf","Hafgan","Hafren","Hafwen","Heddwen","Hedydd","Heini","Heledd","Heulwen","Heulyn","Hydref","Hywela","Iarlles","Idanwen","Ifanna","Ifanwy","Ilar","Iona","Ionor","Iorwen","Iseult","Ithela","Leri","Lili","Lilwen","Liwsi","Lleucu","Llian","Llinos","Llio","Lluan","Lodes","Lowri","Luned","Lyneth","Lynfa","Lynwen","Mabli","Madlen","Maelona","Maelorwen","Mai","Mair","Mallt","Manon","Marged","Medeni","Medi","Medwen","Mefin","Megan","Meinwen","Meirionwen","Melangell","Meleri","Meudwen","Modron","Modwen","Moelwen","Mold","Mona","Morforwyn","Morfudd","Morwen","Mwyndeg","Mwynen","Myfanwy","Nan","Nans","Nant","Nefyn","Nerys","Nest","Nia","Non","Olwen","Owena","Perl","Rebeca","Rhagnell","Rhedyn","Rhiain","Rhiangar","Rhiannon","Rhianwen","Rhianydd","Rhondda","Rhonwen","Rhosan","Rhoslyn","Rhunedd","Rosentyl","Rowena","Sara","Seirial","Seirian","Seren","Siân","Sidan","Sioned","Siriol","Siwan","Siwsan","Sulian","Sulwen","Surwen","Tafwys","Tangwen","Tangwystl","Tanwen","Taryn","Tegan","Tegeirian","Tegfedd","Tegwedd","Tegwen","Tegyd","Teiddwen","Teifi","Telaid","Teleri","Telyn","Tesni","Tirion","Tonwen","Tudful","Undeg","Wena","Wenhaf","Ynyra"
@@ -85,7 +85,7 @@ RealNamesGroups.push(
     },
 
     {
-	label: "Germanic names",
+	label: "Traditional Germanic names",
 	nameSets: [
 	    {
 		// source: http://www.infernaldreams.com/names/Europe/Medieval/Anglo_Saxon.htm
@@ -162,6 +162,31 @@ RealNamesGroups.push(
 	    }
 	]
     },
+
+    {
+	label: "Other traditional names",
+	nameSets: [
+	    {
+		column: "left",
+		label: "Finnish male",
+		splitters: [splitterAfter, splitterBefore],
+		filters: standardFilters,
+		names: [
+		       ]
+	    },
+
+	    {
+		column: "right",
+		label: "Finnish female",
+		splitters: [splitterAfter, splitterBefore],
+		filters: standardFilters,
+		names: [
+		       ]
+	    },
+	]
+    },
+
+
     
     {
 	label: "Angels and Demons",
