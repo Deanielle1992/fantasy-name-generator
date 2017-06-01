69e4fd07663583a378cfb0fcf43a4fe82c46feee
Bartłomiej Sielski <b.sielski@yandex.com>
2017-06-01 19:16:40 +0200

    Add flatten array function

--- collections-helpers.js
+++ collections-helpers.js
@@ -24,3 +24,15 @@ function removeFromArray(array, element) {
         array.splice(index, 1);
     }
 }
+
+function flatten(ary) {
+    var ret = [];
+    for(var i = 0; i < ary.length; i++) {
+        if(Array.isArray(ary[i])) {
+            ret = ret.concat(flatten(ary[i]));
+        } else {
+            ret.push(ary[i]);
+        }
+    }
+    return ret;
+}
